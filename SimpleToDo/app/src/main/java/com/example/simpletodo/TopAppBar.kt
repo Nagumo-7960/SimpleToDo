1626662d6510a13f253a2eed044ba9778372c113
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-24 22:04:26 +0900

    topbarの表示

--- SimpleToDo/app/src/main/java/com/example/simpletodo/TopAppBar.kt
+++ SimpleToDo/app/src/main/java/com/example/simpletodo/TopAppBar.kt
@@ -1,7 +1,11 @@
 package com.example.simpletodo
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -10,5 +14,10 @@ fun TopAppBar() {
         title = {
             Text(text = "Simple ToDo")
         },
+        navigationIcon = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(Icons.Filled.Menu, contentDescription = "Open drawer")
+            }
+        }
     )
 }
