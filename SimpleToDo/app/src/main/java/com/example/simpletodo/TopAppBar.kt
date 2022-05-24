4abfdfac12ff2620dd0c28085c6a5f9ed1fecb98
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-24 22:45:50 +0900

    topbarの背景色と文字、アイコンの色を変更

--- SimpleToDo/app/src/main/java/com/example/simpletodo/TopAppBar.kt
+++ SimpleToDo/app/src/main/java/com/example/simpletodo/TopAppBar.kt
@@ -7,17 +7,26 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun TopAppBar() {
     TopAppBar(
         title = {
-            Text(text = "Simple ToDo")
+            Text(
+                text = "Simple ToDo",
+                color = Color.White
+            )
         },
         navigationIcon = {
             IconButton(onClick = { /* do something */ }) {
-                Icon(Icons.Filled.Menu, contentDescription = "Open drawer")
+                Icon(
+                    Icons.Filled.Menu,
+                    contentDescription = "Open drawer",
+                    tint = Color.White
+                )
             }
-        }
+        },
+        backgroundColor = Color.Blue
     )
 }
